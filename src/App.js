79d0f65d5a63388f3d6ae4316ba87ab3eca4ba02
Edwin Hernandez <edhernandez2@urbe.edu.ve>
2019-10-15 07:44:26 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NavBar from './components/layout/NavBar';
+import Home from './components/home/Home'
+import ProductDetail from './components/product/ProductDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/products/:id' component={ProductDetail} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
